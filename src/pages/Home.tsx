b750054febf635086659f1a9858a2c3b9405d7a4
Igor Marins Santos <igormarinsantos@users.noreply.github.com>
2026-06-20 11:39:00 -0300

    Corrige enquadramento da hero em telas largas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -73,8 +73,8 @@ const heroVisuals = [
   {
     image: homeHeroCompre,
     alt: "Celular e acessórios TecPonto apoiados em uma mão",
-    frameClass: "items-end justify-end pr-0 md:translate-y-8 md:items-center md:pr-0 lg:pr-0",
-    imageClass: "bottom-[21px] left-auto right-[-20px] h-[374px] w-auto max-w-none object-contain md:bottom-auto md:right-auto md:h-auto md:max-h-[700px] md:ml-auto md:w-[82%] lg:max-h-[740px] lg:w-[60%]",
+    frameClass: "items-end justify-end pr-0 md:translate-y-8 md:items-center md:pr-0 lg:pr-0 2xl:translate-y-20",
+    imageClass: "bottom-[21px] left-auto right-[-20px] h-[374px] w-auto max-w-none object-contain md:bottom-auto md:right-auto md:h-auto md:max-h-[700px] md:ml-auto md:w-[82%] lg:max-h-[740px] lg:w-[60%] 2xl:translate-x-[7%]",
     glowClass: "bottom-[-6%] right-[-12%] h-[82%] w-[94%] md:right-[-2%] md:top-[57%] md:h-[78%] md:-translate-y-1/2 md:w-[62%]",
   },
 ];
@@ -316,7 +316,7 @@ const Home = () => {
             </div>
 
             {/* Showcase visual sincronizado com a palavra da hero */}
-            <div className="pointer-events-none absolute inset-x-0 bottom-0 z-0 flex h-[416px] items-end justify-center pt-0 md:pointer-events-auto md:inset-0 md:h-full md:justify-end md:pt-0">
+            <div className="pointer-events-none absolute inset-x-0 bottom-0 z-0 flex h-[416px] items-end justify-center pt-0 md:pointer-events-auto md:inset-y-0 md:left-0 md:right-[calc((100vw-20px-100%)/-2)] md:h-full md:justify-end md:pt-0">
               <motion.div
                 initial={{ opacity: 0, scale: 0.95 }}
                 animate={{ opacity: 1, scale: 1 }}
@@ -344,7 +344,7 @@ const Home = () => {
                       <img
                         src={currentHeroVisual.image}
                         alt={currentHeroVisual.alt}
-                        className={`absolute bottom-0 z-10 object-contain drop-shadow-[0_28px_38px_rgba(0,0,0,0.22)] md:relative md:bottom-auto md:right-auto md:ml-auto ${currentHeroVisual.imageClass}`}
+                        className={`absolute bottom-0 z-10 origin-right object-contain drop-shadow-[0_28px_38px_rgba(0,0,0,0.22)] md:relative md:bottom-auto md:right-auto md:ml-auto 2xl:scale-110 ${currentHeroVisual.imageClass}`}
                       />
                     </motion.div>
                   </AnimatePresence>
